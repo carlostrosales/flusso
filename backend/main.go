173813074e5403c18fe4b11bb7291957bc151823
carlostrosales <rosales.carlost@Gmail.com>
2025-05-09 22:56:43 -0500

    feature: modify main function to start http server

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/carlostrosales/flusso/config"
 	"github.com/carlostrosales/flusso/db"
 	"github.com/carlostrosales/flusso/routes"
 )
 
 func main() {
+	// Load configuration
 	config.LoadConfig()
+
+	// Connect to the database
 	db.Connect()
-	routes.InitRoutes()
+
+	// Initialize routes
+	router := routes.InitRoutes()
+
+	// Start the HTTP server
+	log.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
