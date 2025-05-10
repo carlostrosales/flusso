1d1eb20fd3c6d218e47ae3e30f022b2afcd6729a
carlostrosales <rosales.carlost@Gmail.com>
2025-05-09 22:57:29 -0500

    refactor: replace GORM with standard database/sql for PostgreSQL connection

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,25 +1,25 @@
 package db
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	_ "github.com/lib/pq"
 )
 
-var DB *gorm.DB
+var DB *sql.DB
 
 func Connect() {
-	// Get the database connection details from environment variables
+	// Get the database URL from environment variables
 	dsn := os.Getenv("DB_URL")
 	if dsn == "" {
 		log.Fatalf("DB_URL environment variable is not set")
 	}
 
-	// Connect to the database using GORM
+	// Connect to the database
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = sql.Open("postgres", dsn)
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
