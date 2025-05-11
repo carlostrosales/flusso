0a216b9e040e6ff37854cbd46845ad6452644f3d
carlostrosales <rosales.carlost@Gmail.com>
2025-05-10 19:00:34 -0500

    feat: add Swagger documentation and endpoint to the API

--- backend/main.go
+++ backend/main.go
@@ -4,10 +4,23 @@ import (
 	"log"
 	"net/http"
 
+	_ "github.com/carlostrosales/flusso/docs"
+
+	httpSwagger "github.com/swaggo/http-swagger"
+
 	"github.com/carlostrosales/flusso/config"
 	"github.com/carlostrosales/flusso/db"
 	"github.com/carlostrosales/flusso/routes"
 )
+
+// @title Flusso API
+// @version 1.0
+// @decription This is the API documentation for Flusso
+// @host localhost:8080
+// @BasePath /
+
+// @contact.name API support
+// @contact.email support@flusso.dev
 
 func main() {
 	// Load configuration
@@ -17,9 +30,13 @@ func main() {
 	db.Connect()
 
 	// Initialize routes
-	router := routes.InitRoutes()
+	mux := http.NewServeMux()
+	routes.RegisterRoutes(mux)
+
+	// Add Swagger endpoint
+	mux.Handle("/swagger/", httpSwagger.WrapHandler)
 
 	// Start the HTTP server
 	log.Println("Server is running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
